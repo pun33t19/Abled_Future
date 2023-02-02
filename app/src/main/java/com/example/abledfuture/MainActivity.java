0b5e3a3efdba83e27bd1dc0f6be5f30eb56f3674
Anii232002 <singhaniruddh30@gmail.com>
2023-02-03 02:41:10 +0530

    Swipe

--- app/src/main/java/com/example/abledfuture/MainActivity.java
+++ app/src/main/java/com/example/abledfuture/MainActivity.java
@@ -1,31 +1,68 @@
 package com.example.abledfuture;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 import com.example.abledfuture.databinding.ActivityMainBinding;
+import com.example.abledfuture.fragments.MeetingFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    float x1,x2,y1,y2;
+    private GestureDetectorCompat mDetector;
+
+    ImageView fragImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         setContentView(binding.getRoot());
 
-        RotateAnimation anim = new RotateAnimation(0f, 350f, 15f, 15f);
-        anim.setInterpolator(new LinearInterpolator());
-        anim.setRepeatCount(Animation.INFINITE);
-        anim.setDuration(700);
-
-       // binding.arrowSwipe.startAnimation(anim);
 
     }
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        mDetector.onTouchEvent(event);
+////        System.out.println("HELLO");
+//        MeetingFragment fragment = new MeetingFragment();
+//        Instructions previousFragment = new Instructions();
+//        FragmentTransaction transaction =  getSupportFragmentManager().beginTransaction();
+//        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment).commit();
+//        transaction.remove(previousFragment);
+//        transaction.commit();
+//        return super.onTouchEvent(event);
+//    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        MeetingFragment fragment = new MeetingFragment();
+//        Instructions previousFragment = new Instructions();
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 < x2){
+                        getSupportFragmentManager().beginTransaction().add(R.id.container,fragment).commit();
+            }else if(x1 >= x2){
+                        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment).commit();
+            }
+            break;
+        }
+        return false;
+    }
+
 
 }
--- app/src/main/java/com/example/abledfuture/fragments/MeetingFragment.java
+++ app/src/main/java/com/example/abledfuture/fragments/MeetingFragment.java
@@ -15,9 +15,9 @@ import com.example.Models.MeetModel;
 import com.example.abledfuture.Adapters.MeetAdapter;
 import com.example.abledfuture.databinding.FragmentMeetingBinding;
 
-import org.jitsi.meet.sdk.JitsiMeet;
-import org.jitsi.meet.sdk.JitsiMeetActivity;
-import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+//import org.jitsi.meet.sdk.JitsiMeet;
+//import org.jitsi.meet.sdk.JitsiMeetActivity;
+//import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -44,34 +44,34 @@ public class MeetingFragment extends Fragment {
         binding.recyclerViewMeeting.setLayoutManager(new LinearLayoutManager(getContext()));
         binding.recyclerViewMeeting.setAdapter(adapter);
 
-        URL server;
-        try{
-            server =new URL("https://meet.jit.si");
-            JitsiMeetConferenceOptions defaultOptions=
-                    new JitsiMeetConferenceOptions.Builder()
-                            .setServerURL(server)
-                            .setFeatureFlag("welcomepage.enabled", false).build();
-            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
-
-        }catch (MalformedURLException e){
-            e.printStackTrace();
-        }
-        binding.scheduleMeetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-                        .setRoom("ksjijijsddk")
-                        .setFeatureFlag("welcomepage.enabled", false).build();
-                JitsiMeetActivity.launch(getContext(),options);
-            }
-        });
-
-        /*binding.scheduleMeetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getContext(), MeetActivity.class));
-            }
-        });*/
+//        URL server;
+//        try{
+//            server =new URL("https://meet.jit.si");
+//            JitsiMeetConferenceOptions defaultOptions=
+//                    new JitsiMeetConferenceOptions.Builder()
+//                            .setServerURL(server)
+//                            .setFeatureFlag("welcomepage.enabled", false).build();
+//            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+//
+//        }catch (MalformedURLException e){
+//            e.printStackTrace();
+//        }
+//        binding.scheduleMeetBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+//                        .setRoom("ksjijijsddk")
+//                        .setFeatureFlag("welcomepage.enabled", false).build();
+//                JitsiMeetActivity.launch(getContext(),options);
+//            }
+//        });
+//
+//        /*binding.scheduleMeetBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(getContext(), MeetActivity.class));
+//            }
+//        });*/
 
     }
 
